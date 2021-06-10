e97472a9f5a1a2eb9f9d497b50abce27f79ca556
sivaraman <ssivaraman@yandex.com>
2021-06-10 21:20:52 +0530

    init commit

---
+++ cart/dist/src_index_js.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcart"] = self["webpackChunkcart"] || []).push([["src_index_js"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const cartText = \"<div>You have 3 elements in your cart</div>\"; \r\n\r\ndocument.querySelector(\"cart-dev\").innerHTML = cartText;\n\n//# sourceURL=webpack://cart/./src/index.js?");
+
+/***/ })
+
+}]);
---
+++ container/dist/src_bootstrap_js.js
@@ -0,0 +1,22 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcontainer"] = self["webpackChunkcontainer"] || []).push([["src_bootstrap_js"],{
+
+/***/ "./src/bootstrap.js":
+/*!**************************!*\
+  !*** ./src/bootstrap.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var products_ProductsIndex__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! products/ProductsIndex */ \"webpack/container/remote/products/ProductsIndex\");\n/* harmony import */ var products_ProductsIndex__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(products_ProductsIndex__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var cart_CartShow__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! cart/CartShow */ \"webpack/container/remote/cart/CartShow\");\n/* harmony import */ var cart_CartShow__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(cart_CartShow__WEBPACK_IMPORTED_MODULE_1__);\n\r\n\r\n\r\n\r\nconsole.log(\"From the container\");\n\n//# sourceURL=webpack://container/./src/bootstrap.js?");
+
+/***/ })
+
+}]);
